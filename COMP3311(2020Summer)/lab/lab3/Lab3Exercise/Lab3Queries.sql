fdeafbbabb6aeb75ec78ace0c75947932db50bb0
Sam Li <55217028+wsli1998@users.noreply.github.com>
2020-07-11 09:30:52 +0800

    finish lab3

--- COMP3311(2020Summer)/lab/lab3/Lab3Exercise/Lab3Queries.sql
+++ COMP3311(2020Summer)/lab/lab3/Lab3Exercise/Lab3Queries.sql
@@ -10,6 +10,11 @@ select 'Query 1: Find the student id, first name, last name, email and cga of th
        '         are in the ELEC department. Order the result by cga from highest to lowest cga.' from dual;
 set heading on
 -- ***** Construct Query 1 below this line *****
+select studentid,firstName,Lastname,email,cga
+from Student 
+where departmentid='ELEC' 
+order by cga desc;
+
 
 
 /* Query 2 */
@@ -20,6 +25,10 @@ select 'Query 2: Find the first name of the students whose first name' || chr(10
 set heading on
 -- ***** Construct Query 2 below this line *****
 
+select *
+from student 
+where  firstname like '__l%';
+
 
 /* Query 3 */
 set heading off
@@ -28,6 +37,10 @@ select 'Query 3: Find the last name of the students whose last name' || chr(10) 
        '         contains either the letter "c" or the letter "z".' from dual;
 set heading on
 -- ***** Construct Query 3 below this line *****
+select lastname
+from student 
+where  lastname like '%c%' or lastname like '%z%' ;
+
 
 
 /* Query 4 */
@@ -37,6 +50,10 @@ select 'Query 4: Find the first name and last name of all students whose first n
        '         or last name contains a double letter (e.g., "ee", "ll", "mm", etc.).' from dual;
 set heading on
 -- ***** Construct Query 4 below this line *****
+select firstname , lastname
+from student
+where regexp_like (lastName,'([a-z])\1','i') or regexp_like(firstName,'([a-z])\1','i') ;
+
 
 
 /* Query 5 */
@@ -48,3 +65,8 @@ select 'Query 5: Find the student id, first name, last name, cga and department 
 set heading on
 -- ***** Construct Query 5 below this line *****
 
+select studentID,firstname,lastname,cga ,departmentid
+from student
+where (departmentid='COMP' or departmentid='ELEC')
+and cga not between 2.5 and 3.5
+order by lastname asc;
